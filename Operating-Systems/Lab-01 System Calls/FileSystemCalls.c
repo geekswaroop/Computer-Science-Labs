cdf0e9c9fb2a8fd86f5ace7b835ef590ee39d976
geekswaroop <kkrishnaswaroop99@gmail.com>
2020-01-07 17:17:18 +0530

    Added Lab 1

--- Operating-Systems/Lab-01 System Calls/FileSystemCalls.c
+++ Operating-Systems/Lab-01 System Calls/FileSystemCalls.c
@@ -4,88 +4,97 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <stdio.h>
+
 int main()
 {
-	int fd;
-	char *filename = "/home/student/Documents/181CO125/file.txt";
-	char *filename2 = "/home/student/Documents/181CO125/file2.txt";
+    int fd;
+    char* filename = "/home/student/181CO125/file.txt";
+    char* filename2 = "/home/student/181CO125/file2.txt";
 
+    //CREAT
+    //S_IRWXU means user had Read, Write, and Execute permission
+    fd = creat(filename, S_IRWXU);
+    if(fd<0)
+        perror("Error");
+    else
+    {
+        printf("File created and opened successfully with fd number: %d\n", fd);
+    }
+    //OPEN
+    fd = open(filename, O_CREAT | O_RDWR, S_IRWXU);
+    if(fd<0)
+        perror("Error");
+    else
+    {
+        printf("\nFile opened successfully with fd = %d\n", fd);     
+    }
+    
+    //CLOSE
+    int close_value = close(fd);
+    if(close_value!=0)
+        perror("Error");
+    else
+    {
+        printf("\nFile Closed Successfully\n");
+    }
+    //Opening file again for READ operations
+    fd = open(filename, O_CREAT | O_RDWR, S_IRWXU);
+    if(fd<0)
+        perror("Error");
+    else
+    {
+        printf("\nFile opened successfully with fd = %d\n", fd);     
+    }
+    
+    //READ
+    char buf[100];
+    int size_read = read(fd, buf, 100); //Reads 100 bytes from the buf
+    buf[size_read]='\0';
+    if(size_read<0)
+         perror("Error");
+    else
+    {
+        printf("\n%d bytes of %s File successfully read\n", size_read, buf);
+    }
 
-	//CREAT
-	fd = creat(filename, S_IRWXU);
-	if(fd<0)
-		printf("Error in creating a file\n");
+    //WRITE, writes to a File Descriptor
+    char str[] = "Hello! Working with write functions!";
+    int size_written = write(fd,str,strlen(str));
+
+       printf("Content \"%s\" written to file of size %d bytes.\n", str, size_written);
+
+    
+    //LSEEK
+    lseek(fd,0,SEEK_SET);
+
+    //DUP
+    int duplicated = dup(fd);
+    if(duplicated<0)
+        perror("Error");
+    else
+    {
+        printf("\nOld FD=%d, New FD=%d\n", fd ,duplicated);
+    }
+    //LINK
+	int link_value = link(filename, filename2);
+	if(link_value!=0)
+		perror("\nError");
 	else
-		printf("Created a file.txt successfully\n");
-
-	//OPEN
-	fd = open(filename, O_CREAT | O_RDWR, S_IRWXU);
-	if(fd<0)
-		printf("Error in opening a file\n");
-	else
-		printf("Opened file.txt successfully\n");
-
-	//WRITE
-	char *str = "Hello! I'm trying out some system calls";
-	int size_wrote = write(fd, str, strlen(str));
-	if(size_wrote<0)
-		perror("Error");
-	else
-		printf("Wrote \"%s\" to file of size %d bytes\n", str, size_wrote);
-
-	//LSEEK
-	lseek(fd, 0, SEEK_SET);
-
-	//READ
-	char buf[100];
-	int size_read = read(fd, buf, 100);
-	buf[size_read]='\0';
-	if(size_read<0)
-		perror("Error");
-	else
-		printf("Successully read \"%s\" from file of %d size\n", buf, size_read);
-
-	//Dup
-	int new_fd = dup(fd);
-	if(new_fd<0)
-		perror("Error");
-	else
-		printf("Old descriptor %d, new descriptor %d\n", fd, new_fd);
-	//LINK
-	int linked = link(filename, filename2);
-	if(linked!=0)
 	{
-		perror("Error");
-	}
-	else
-	{
-		printf(" link created successfully\n");
+		printf("\nLink created successfully\n");
+
 		//UNLINK
-		int unlink_fd = unlink(filename2);
-		if(unlink!=0)
-			perror("Error");
+		int stat = unlink(filename2);
+		if(stat == 0)
+			printf("Link removed successfully\n\n");
 		else
-		{
-			printf("Unlinked successfully\n");
-
-		}
-
-
-	}
-
-	//ACCESS
-	int accessed = access(filename, F_OK);
-	if(accessed<0)
-		perror("Error");
+			perror("\nError");
+    //ACCESS
+	int exists = access(filename, F_OK);
+	if(exists < 0)
+		perror("error");
 	else
-		printf("File %s can be accessed\n", filename);
+        printf("File \"%s\" exists\n", filename);
+}
 
-	//CHMOD
-	int chmod_permission = chmod(filename, S_IRWXG| S_IRWXU);
-	if(chmod_permission!=0)
-		perror("Error");
-	else
-		printf("Permissions to the file working well!\n");
-
-	
 }
